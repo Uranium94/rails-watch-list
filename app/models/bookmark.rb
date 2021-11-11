70067924df1c333378b8d6dabf93c009666b230c
Max Ellis <uraniumax@gmail.com>
2021-11-11 14:06:31 +0100

    bookmark, list and movie models created

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,10 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
-  belongs_to :bookmarks
-  validates [:movie_id, :list_id], uniqueness: true, presence: true
+  belongs_to :list
+
+  validates :movie_id, presence: true
+  validates :list_id, presence: true
+  validates :list_id, uniqueness: { scope: :movie_id }
+
   validates :comment, length: { minimum: 6 }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+  validates :overview, length: { minimum: 1 }
+  validates :title, length: { minimum: 6 }
 end
